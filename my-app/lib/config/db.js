0787ef8f719bdd015e18b3d302718c232e9d60d1
gokulgopandevelopshere <147742215+gokulgopandevelopshere@users.noreply.github.com>
2024-07-10 03:30:03 +0300

    Update db.js

--- my-app/lib/config/db.js
+++ my-app/lib/config/db.js
@@ -1,6 +1,7 @@
 import mongoose from 'mongoose'
 
 export const ConnectDb = async() =>{
-    await mongoose.connect('mongodb+srv://admin:admin123@cluster0.mqoam1b.mongodb.net/Blogger-next')
+    await mongoose.connect('mongodb+srv://admin:admin123@cluster0.mqoam1b.mongodb.net/')
     console.log('DB connected')
+    //mongodb+srv://admin:admin123@cluster0.mqoam1b.mongodb.net/
 }   
